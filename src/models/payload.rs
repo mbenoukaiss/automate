c73cbad5a39771b9b93e7f15ec55b8a6db1b2579
mbenoukaiss <marwanebenoukaiss@aol.com>
2019-09-16 11:34:00 +0200

    Add more events

--- src/models/payload.rs
+++ src/models/payload.rs
@@ -12,7 +12,7 @@ pub struct Payload<D> where D: FromJson {
 }
 
 #[payload(op = 0, event = "READY", server)]
-pub struct DispatchReady {
+pub struct ReadyDispatch {
     pub v: u16,
     pub user: User,
     pub private_channels: Vec<Channel>,
@@ -22,7 +22,55 @@ pub struct DispatchReady {
 }
 
 #[payload(op = 0, event = "GUILD_CREATE", server)]
-pub struct DispatchGuildCreate(pub Guild);
+pub struct GuildCreateDispatch(pub Guild);
+
+#[payload(op = 0, event = "GUILD_UPDATE", server)]
+pub struct GuildUpdateDispatch(pub Guild);
+
+#[payload(op = 0, event = "GUILD_DELETE", server)]
+pub struct GuildDeleteDispatch {
+    pub id: u64,
+    pub unavailable: Option<bool>
+}
+
+#[payload(op = 0, event = "GUILD_BAN_ADD", server)]
+pub struct GuildBanAddDispatch {
+    pub guild_id: u64,
+    pub user: User
+}
+
+#[payload(op = 0, event = "GUILD_BAN_REMOVE", server)]
+pub struct GuildBanRemoveDispatch {
+    pub guild_id: u64,
+    pub user: User
+}
+
+#[payload(op = 0, event = "GUILD_MEMBER_ADD", server)]
+pub struct GuildMemberAddDispatch {
+    pub guild_id: u64,
+    pub user: User,
+    pub nick: Option<Nullable<String>>,
+    pub roles: Vec<u64>,
+    pub joined_at: String,
+    pub premium_since: Option<Nullable<String>>,
+    pub hoisted_role: Nullable<bool>, //TODO: get the right type
+    pub deaf: bool,
+    pub mute: bool
+}
+
+#[payload(op = 0, event = "GUILD_MEMBER_REMOVE", server)]
+pub struct GuildMemberRemoveDispatch {
+    pub guild_id: u64,
+    pub user: User
+}
+
+#[payload(op = 0, event = "GUILD_MEMBER_UPDATE", server)]
+pub struct GuildMemberUpdateDispatch {
+    pub guild_id: u64,
+    pub roles: Vec<u64>,
+    pub user: User,
+    pub nick: String
+}
 
 /// This payload should not be handled by bots
 /// as it will always be empty and does not mean
@@ -30,29 +78,62 @@ pub struct DispatchGuildCreate(pub Guild);
 ///
 /// More information in [this issue](https://github.com/discordapp/discord-api-docs/issues/683)
 #[payload(op = 0, event = "PRESENCES_REPLACE", server)]
-pub struct DispatchPresencesReplace(pub Vec<PresenceUpdate>);
+pub struct PresencesReplaceDispatch(pub Vec<PresenceUpdate>);
 
 #[payload(op = 0, event = "PRESENCE_UPDATE", server)]
-pub struct DispatchPresenceUpdate(pub PresenceUpdate);
+pub struct PresenceUpdateDispatch(pub PresenceUpdate);
 
 #[payload(op = 0, event = "MESSAGE_CREATE", server)]
-pub struct DispatchMessageCreate(pub Message);
+pub struct MessageCreateDispatch(pub Message);
 
 #[payload(op = 0, event = "MESSAGE_UPDATE", server)]
-pub struct DispatchMessageUpdate(pub Message);
+pub struct MessageUpdateDispatch(pub Message);
 
 #[payload(op = 0, event = "MESSAGE_DELETE", server)]
-pub struct DispatchMessageDelete {
+pub struct MessageDeleteDispatch {
     pub id: u64,
     pub channel_id: u64,
     pub guild_id: Option<u64>
 }
 
 #[payload(op = 0, event = "MESSAGE_DELETE_BULK", server)]
-pub struct DispatchMessageDeleteBulk {
+pub struct MessageDeleteBulkDispatch {
     pub ids: Vec<u64>,
     pub channel_id: u64,
     pub guild_id: Option<u64>
+}
+
+#[payload(op = 0, event = "MESSAGE_REACTION_ADD", server)]
+pub struct MessageReactionAddDispatch {
+    pub guild_id: Option<u64>,
+    pub channel_id: u64,
+    pub user_id: u64,
+    pub message_id: u64,
+    pub emoji: PartialEmoji
+}
+
+#[payload(op = 0, event = "MESSAGE_REACTION_REMOVE", server)]
+pub struct MessageReactionRemoveDispatch {
+    pub guild_id: Option<u64>,
+    pub channel_id: u64,
+    pub user_id: u64,
+    pub message_id: u64,
+    pub emoji: PartialEmoji
+}
+
+#[payload(op = 0, event = "MESSAGE_REACTION_REMOVE_ALL", server)]
+pub struct MessageReactionRemoveAllDispatch {
+    pub guild_id: Option<u64>,
+    pub channel_id: u64,
+    pub message_id: u64
+}
+
+#[payload(op = 0, event = "TYPING_START", server)]
+pub struct TypingStartDispatch {
+    pub guild_id: Option<u64>,
+    pub channel_id: u64,
+    pub user_id: u64,
+    pub member: GuildMember
 }
 
 #[payload(op = 1, client)]
