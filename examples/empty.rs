15a8667e816822ee89e464282ce660bd5d7ee048
mbenoukaiss <marwanebenoukaiss@aol.com>
2019-11-29 10:46:37 +0100

    Update version and license

--- examples/empty.rs
+++ examples/empty.rs
@@ -1,9 +1,25 @@
+use automate::{async_trait, Listener, Session};
 use automate::{Error, Discord};
 use std::env;
+use automate::gateway::MessageReactionAddDispatch;
+
+struct MessageListener;
+
+#[async_trait]
+impl Listener for MessageListener {
+    async fn on_reaction_add(&mut self, session: &Session, data: &MessageReactionAddDispatch) -> Result<(), Error> {
+        if data.user_id != session.bot().id {
+            session.create_reaction(data.channel_id, data.message_id, &data.emoji).await?;
+        }
+
+        Ok(())
+    }
+}
 
 fn main() -> Result<!, Error> {
     automate::setup_logging();
 
     Discord::new(&env::var("DISCORD_API_TOKEN").expect("API token not found"))
+        .with_listener(Box::new(MessageListener))
         .connect_blocking()
 }
